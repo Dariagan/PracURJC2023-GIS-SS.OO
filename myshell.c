f6b3ac21e1e0b6cf70584555c0e255209a937edf
Dariagan <seomasini@gmail.com>
2023-12-01 00:22:40 +0100

    estado funcional con n-1 tubos

--- myshell.c
+++ myshell.c
@@ -11,6 +11,19 @@
 #include <signal.h>
 #include <time.h>
 
+void close_non_adyacent_pipes(int ** pipes, const int my_i, const int N_COMMANDS)
+{
+    int j;
+    const int N_PIPES = N_COMMANDS - 1;
+    for(j = my_i + 1; j < N_PIPES; j++)
+    {
+        close_entire_pipe(pipes[j]);
+    }
+    for(j = my_i - 2; j >= 0; j--)
+    {
+        close_entire_pipe(pipes[j]);
+    }
+}
 
 void close_entire_pipe(const int pipe[2])
 {close(pipe[0]); close(pipe[1]);}
@@ -26,19 +39,19 @@ int execute_line(tline * line)
     char const *native_commands[] = {};
     pid_t pid;
     const unsigned int N_COMMANDS = line->ncommands;
-
-    char buf[2048];
+    int i, j;
 
     //writing end: 1, reading end: 0
-    int pipes[2][2];
-    bool first_is_echo;
+    int **pipes;
+    //NO OLVIDARSE DE FREEAR DESP
+    pipes = (int **)malloc((N_COMMANDS-1)*sizeof(int**));
 
-    int i;
-    //todo error handling
-    pipe(pipes[0]);
-    pipe(pipes[1]);
+    for(i = 0; i < N_COMMANDS - 1; i++)
+    {
+        pipes[i] = (int *)malloc(2*sizeof(int*));
+        pipe(pipes[i]);
+    }
     
-    //for (i = N_COMMANDS - 1; i >= 0; i--)
     for(i = 0; i < N_COMMANDS; i++)
     {
         //todo error handling
@@ -46,40 +59,37 @@ int execute_line(tline * line)
 
         if(pid == 0)//child
         {
-            printf("i am %d entering\n", i);
-            fflush(stdout);
-            if(i == 0 && i == N_COMMANDS - 1)
+            fprintf(stderr, "i am %d entering\n", i);
+            fflush(stderr);
+            if(i == 0 && N_COMMANDS > 1)
             {
-                close_entire_pipe(pipes[0]);
-                close_entire_pipe(pipes[1]);
-            }
-            else if(i == 0)
-            {
-                close_entire_pipe(pipes[0]);
+                close(pipes[i][READING_END]);
+                dup2(pipes[i][WRITING_END], STDOUT_FILENO);
+                close(pipes[i][WRITING_END]);
                 
-                close(pipes[1][READING_END]);
-                dup2(pipes[1][WRITING_END], STDOUT_FILENO);
-                close(pipes[1][WRITING_END]);
+                close_non_adyacent_pipes(pipes, i, N_COMMANDS);
             }
             else if(i < N_COMMANDS - 1)
             {
-                //i == 1
-                close(pipes[i%2][WRITING_END]);
-                dup2(pipes[i%2][READING_END], STDIN_FILENO);
-                close(pipes[i%2][READING_END]);
-                
-                close(pipes[i%2 - 1][READING_END]);
-                dup2(pipes[i%2 - 1][WRITING_END], STDOUT_FILENO);
-                close(pipes[i%2 - 1][WRITING_END]);
-            }
-            else 
-            {
-                close(pipes[i%2][WRITING_END]);
-                dup2(pipes[i%2][READING_END], STDIN_FILENO);
-                close(pipes[i%2][READING_END]);
+                close(pipes[i - 1][WRITING_END]);
+                dup2(pipes[i - 1][READING_END], STDIN_FILENO);
+                close(pipes[i - 1][READING_END]);
 
-                close_entire_pipe(pipes[i%2 - 1]);
+                close(pipes[i][READING_END]);
+                dup2(pipes[i][WRITING_END], STDOUT_FILENO);
+                close(pipes[i][WRITING_END]);
+
+                close_non_adyacent_pipes(pipes, i, N_COMMANDS);
             }
+            else
+            {
+                close(pipes[i - 1][WRITING_END]);
+                dup2(pipes[i - 1][READING_END], STDIN_FILENO);
+                close(pipes[i - 1][READING_END]);
+
+                close_non_adyacent_pipes(pipes, i, N_COMMANDS);
+            }
+            
             fprintf(stderr, "i am %d, executing\n", i);
             fflush(stderr);
             execvp(line->commands[i].filename, line->commands[i].argv);
@@ -88,24 +98,18 @@ int execute_line(tline * line)
          
         }
     }
-    close_entire_pipe(pipes[0]);
-    close_entire_pipe(pipes[1]);
+    for(i = 0; i < N_COMMANDS - 1; i++)
+    {
+        close_entire_pipe(pipes[i]);
+    }
     
     for(i = 0; i < N_COMMANDS; i++)
     {
         wait(NULL);
-        if (i == 0)
-        {
-
-        }
-
         printf("%d died\n", i);
         fflush(stdout);
     }
-    
-
-    //creo q hay q tratar el caso comando i=0 echo, agregarle un EOF
-    
+        
     return 0;
 }
 
@@ -124,29 +128,3 @@ int main(int argc, char const *argv[])
     }
     return 0;
 }
-// if (i == 0 && i != N_COMMANDS - 1)
-            // {
-            //     close_entire_pipe(pipelef);
-            //     close(pipe_RIGHT[0]);
-            //     dup2(pipe_RIGHT[1], STDOUT_FILENO);
-            //     close(pipe_RIGHT[1]);
-            // }
-            // else if (i % 2 == 1)
-            // {
-            //     close(pipe_RIGHT[1]);
-            //     dup2(pipe_RIGHT[0], STDIN_FILENO);
-            //     close(pipe_RIGHT[0]);
-            //     close(pipelef[0]);
-            //     dup2(pipelef[1], STDOUT_FILENO);
-            //     close(pipelef[1]);
-            // }   
-            // else if (i % 2 == 0)
-            // {
-            //     close(pipelef[1]);
-            //     dup2(pipelef[0], STDIN_FILENO);
-            //     close(pipelef[0]);
-
-            //     close(pipe_RIGHT[0]);
-            //     dup2(pipe_RIGHT[1], STDOUT_FILENO);
-            //     close(pipe_RIGHT[1]);
-            // } 
